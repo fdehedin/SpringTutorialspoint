835cd4cdbca1337ca986983f958b8bec9f421abd
FDN <FDN@CHNBFDN02.belsoftnet.local>
2016-10-27 13:15:23 +0200

    file and console log4j appender, correction for tomcat (url pattern)

--- src/com/tutorialspoint/WebController.java
+++ src/com/tutorialspoint/WebController.java
@@ -4,20 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.ModelMap;
+import org.apache.log4j.Logger;
 
 @Controller
 // @RequestMapping("/")
 public class WebController {
 
+	private static final Logger log = Logger.getLogger(WebController.class.getName());
+
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String printHello(ModelMap model) {
 		model.addAttribute("message", "Hello Spring MVC Framework!");
-
+	
 		return "hello";
 	}
 
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String index() {
+		System.out.println(System.getProperty("${catalina.base}")  + "  /  "+System.getProperty("log4j.appender.FILE.File")); 
+		log.info("Going to printHello");
 		return "index";
 	}
 
